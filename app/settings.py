133268624171640727f8d10a3a1f5868fc68344f
Joubert Diego <jdrsjdrsoares@gmail.com>
2024-07-21 21:22:21 +0200

    ajuste settings

--- app/settings.py
+++ app/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0z+dhzqc#ncj8md=5k+lht6poctc2rly22u2mea8)5q54ap+8p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
